80b27f471dd870067f8311fc0e0998bff68381e3
ferez96 <minh9a4@gmail.com>
2016-10-07 05:24:15 +0700

    Thiết kế gói
    README.md
    Client.Package

--- src/Client/Package.java
+++ src/Client/Package.java
@@ -6,4 +6,17 @@ import Protocol.IPackage;
  * Created by DuongThaiMinh on 07/10/2016.
  */
 public class Package extends IPackage{
+    public Package(int source, int destination, int dataPos, int flag, int option, byte[] data) {
+        super(source, destination, dataPos, flag, option, data);
+    }
+
+    @Override
+    public void push() {
+
+    }
+
+    @Override
+    public void get() {
+
+    }
 }
